fc82c82ca039cda32c682285b75f498a031178a8
meanmon13 <meanmon13@gmail.com>
2016-08-20 00:04:02 -0400

    Fixed tabs

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,36 +1,37 @@
-var roleUpgrader = 
+var roleUpgrader =
 {
 
-    /** @param {Creep} creep **/
-    run: function(creep) 
+   /** @param {Creep} creep **/
+   run: function(creep)
 	{
 
-        if(creep.memory.upgrading && creep.carry.energy == 0) 
+      if(creep.memory.upgrading && creep.carry.energy == 0)
 		{
-            creep.memory.upgrading = false;
-            creep.say('harvesting');
-        }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) 
+         creep.memory.upgrading = false;
+         creep.say('harvesting');
+      }
+      if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity)
 		{
-            creep.memory.upgrading = true;
-            creep.say('upgrading');
-        }
+         creep.memory.upgrading = true;
+         creep.say('upgrading');
+      }
 
-        if(creep.memory.upgrading) 
+      if(creep.memory.upgrading)
 		{
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) 
+         if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE)
 			{
-                creep.moveTo(creep.room.controller);
-            }
-        }
-        else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) 
+            creep.moveTo(creep.room.controller);
+         }
+      }
+      else
+      {
+         var sources = creep.room.find(FIND_SOURCES);
+         if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
 			{
-                creep.moveTo(sources[0]);
-            }
-        }
-    }
+            creep.moveTo(sources[0]);
+         }
+      }
+   }
 };
 
 module.exports = roleUpgrader;
